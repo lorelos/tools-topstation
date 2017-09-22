222bc294e1982c141f0aad4059348904c2d5869e
Aurelien Di-Meo <aurelien.di-meo@province-sud.nc>
2017-09-23 07:51:33 +1100

    Commit initial

---
+++ spec/tools-topstation-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ToolsTopstationView from '../lib/tools-topstation-view';
+
+describe('ToolsTopstationView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
